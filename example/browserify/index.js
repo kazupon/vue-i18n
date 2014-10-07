409c073b70da05740203f62ddcc71124568a126e
kazuya kawaguchi <kawakazu80@gmail.com>
2014-10-08 00:49:07 +0900

    Add browserify example

---
+++ example/browserify/index.js
@@ -0,0 +1,27 @@
+var Vue = require('vue')
+var i18n = require('vue-i18n')
+
+// ready translated locales
+var locales = {
+  en: {
+    message: {
+      hello: 'the world'
+    }
+  },
+  ja: {
+    message: {
+      hello: 'ザ・ワールド'
+    }
+  }
+}
+
+// set plugin
+Vue.use(i18n, {
+  lang: 'ja',
+  locales: locales
+})
+
+// create instance
+new Vue({
+  el: '#test-i18n'
+})
