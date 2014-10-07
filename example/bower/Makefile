5c7833c61c42a7d463bd9d89c129098f9ff1ba18
kazuya kawaguchi <kawakazu80@gmail.com>
2014-10-08 00:31:30 +0900

    Add bower example

---
+++ example/bower/Makefile
@@ -0,0 +1,12 @@
+BOWER = bower
+
+install: bower.json
+	@$(BOWER) install
+
+clean:
+	@rm -rf bower_components
+
+open:
+	@open index.html
+
+.PHONY: clean
---
+++ example/bower/index.js
@@ -0,0 +1,26 @@
+var i18n = window['vue-i18n']
+
+// ready translated locales
+var locales = {
+  en: {
+    message: {
+      hello: 'the world'
+    }
+  },
+  ja: {
+    message: {
+      hello: 'ザ・ワールド'
+    }
+  }
+}
+
+// set plugin
+Vue.use(i18n, {
+  lang: 'ja',
+  locales: locales
+})
+
+// create instance
+new Vue({
+  el: '#test-i18n'
+})
