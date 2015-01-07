595cdbf0aa48dd681c91c18f0071e1c0d4aec489
kazuya kawaguchi <kawakazu80@gmail.com>
2015-01-08 00:57:33 +0900

    Update v-t spec

--- test/specs/index.js
+++ test/specs/index.js
@@ -48,110 +48,156 @@ describe('i18n', function () {
   })
 
 
-  /*
   describe('v-t', function () {
+    var vm, targetVM
+
+    before(function () {
+      Vue.config.async = false
+    })
+
+    after(function () {
+      Vue.config.async = true
+    })
+
     describe('basic', function () {
       describe('en', function () {
         it('should translate an english', function () {
-          mock('translate-en', '<p v-t="message.hello"></p>')
-
           Vue.use(i18n, {
             lang: 'en',
             locales: locales
           })
+          
+          var ViewModel = Vue.extend({
+            template: '<p v-t="message.hello"></p>',
+            el: function () {
+              var el = document.createElement('div')
+              el.id = 'translate-en'
+              document.body.appendChild(el)
+              return el
+            }
+          })
+          new ViewModel()
 
-          new Vue({ el: '#translate-en' })
-
-          var el = document.querySelector('#translate-en')
-          expect(el.textContent).to.be.eql(locales.en.message.hello)
+          expect(document.querySelector('#translate-en p').textContent)
+            .to.be.eql(locales.en.message.hello)
         })
       })
 
       describe('ja', function () {
         it('should translate a japanese', function () {
-          mock('translate-ja', '<p v-t="message.hello"></p>')
-
           Vue.use(i18n, {
             lang: 'ja',
             resources: locales
           })
 
-          new Vue({ el: '#translate-ja' })
+          var ViewModel = Vue.extend({
+            template: '<p v-t="message.hello"></p>',
+            el: function () {
+              var el = document.createElement('div')
+              el.id = 'translate-ja'
+              document.body.appendChild(el)
+              return el
+            }
+          })
+          new ViewModel()
 
-          var el = document.querySelector('#translate-ja')
-          expect(el.textContent).to.be.eql(locales.ja.message.hello)
+          expect(document.querySelector('#translate-ja p').textContent)
+            .to.be.eql(locales.ja.message.hello)
         })
       })
     })
 
     describe('lang resource not found', function () {
       it('should not translate', function () {
-        mock('translate-it', '<p v-t="message.hello"></p>')
-
         Vue.use(i18n, {
           lang: 'it',
           locales: locales
         })
 
-        new Vue({ el: '#translate-it' })
+        var ViewModel = Vue.extend({
+          template: '<p v-t="message.hello"></p>',
+          el: function () {
+            var el = document.createElement('div')
+            el.id = 'translate-it'
+            document.body.appendChild(el)
+            return el
+          }
+        })
+        new ViewModel()
 
-        var el = document.querySelector('#translate-it')
-        expect(el.textContent).to.be.eql('message.hello')
+        expect(document.querySelector('#translate-it p').textContent)
+          .to.be.eql('message.hello')
       })
     })
 
     describe('resource key not found', function () {
       it('should not translate', function () {
-        mock('translate-en-key-nothing', '<p v-t="message.foo"></p>')
-
         Vue.use(i18n, {
           lang: 'en',
           locales: locales
         })
 
-        new Vue({ el: '#translate-en-key-nothing' })
+        var ViewModel = Vue.extend({
+          template: '<p v-t="message.foo"></p>',
+          el: function () {
+            var el = document.createElement('div')
+            el.id = 'translate-en-key-nothing'
+            document.body.appendChild(el)
+            return el
+          }
+        })
+        new ViewModel()
 
-        var el = document.querySelector('#translate-en-key-nothing')
-        expect(el.textContent).to.be.eql('message.foo')
+        expect(document.querySelector('#translate-en-key-nothing p').textContent)
+          .to.be.eql('message.foo')
       })
     })
 
     describe('resource key empty', function () {
       it('should not translate', function () {
-        mock('translate-key-empty', '<p v-t=""></p>')
-
         Vue.use(i18n, {
           lang: 'en',
           locales: locales
         })
 
-        new Vue({ el: '#translate-key-empty' })
+        var ViewModel = Vue.extend({
+          template: '<p v-t=""></p>',
+          el: function () {
+            var el = document.createElement('div')
+            el.id = 'translate-key-empty'
+            document.body.appendChild(el)
+            return el
+          }
+        })
+        new ViewModel()
 
-        var el = document.querySelector('#translate-key-empty')
-        expect(el.textContent).to.be.eql('')
+        expect(document.querySelector('#translate-key-empty p').
+          textContent).to.be.eql('')
       })
     })
 
     describe('translate component module', function () {
       it('should translate', function () {
-        mock(
-          'translate-parent',
-          '<div><p v-t="message.hello"></p><div v-component="hoge"></div></div>'
-        )
-
         Vue.use(i18n, {
           lang: 'en',
           locales: locales
         })
 
-        new Vue({
-          el: '#translate-parent',
+        var ViewModel = Vue.extend({
+          template: '<div><p v-t="message.hello"></p><div v-component="hoge"></div></div>',
+          el: function () {
+            var el = document.createElement('div')
+            el.id = 'translate-parent'
+            document.body.appendChild(el)
+            return el
+          },
           components: {
             hoge: Vue.extend({
               template: '<p id="translate-child" v-t="message.hoge"></p>'
             })
           }
         })
+        new ViewModel()
 
         var child_el = document.querySelector('#translate-child')
         expect(child_el.textContent).to.be.eql(locales.en.message.hoge)
@@ -161,5 +207,4 @@ describe('i18n', function () {
       })
     })
   })
-  */
 })
