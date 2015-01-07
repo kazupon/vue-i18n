8a474a0e60e082b84dc28837f95f440dca5af7f6
kazuya kawaguchi <kawakazu80@gmail.com>
2015-01-08 01:23:58 +0900

    Add dist files

--- dist/vue-i18n.js
+++ dist/vue-i18n.js
@@ -1,108 +1,106 @@
-
-;(function(){
-
 /**
- * Require the module at `name`.
- *
- * @param {String} name
- * @return {Object} exports
- * @api public
+ * vue-i18n v1.0.0
+ * (c) 2015 kazuya kawaguchi
+ * Released under the MIT License.
  */
 
-function require(name) {
-  var module = require.modules[name];
-  if (!module) throw new Error('failed to require "' + name + '"');
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define(factory);
+	else if(typeof exports === 'object')
+		exports["vue-i18n"] = factory();
+	else
+		root["vue-i18n"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
 
-  if (!('exports' in module) && typeof module.definition === 'function') {
-    module.client = module.component = true;
-    module.definition.call(this, module.exports = {}, module);
-    delete module.definition;
-  }
+	/**
+	 * Expose internationalization plugin
+	 *
+	 * @param {Object} Vue
+	 * @param {Object} opts
+	 */
 
-  return module.exports;
-}
+	module.exports = function (Vue, opts) {
+	  opts = opts || {}
+	  var lang = opts.lang || 'en'
+	  var locales = opts.locales || opts.resources || {}
 
-/**
- * Registered modules.
- */
+	  Vue.t = function (key) {
+	    var ret = key || ''
+	    var locale = locales[lang]
+	    if (key && locale) {
+	      var namespaces = key.split('.')
+	      for (var i = 0; i < namespaces.length; i++) {
+	        locale = locale[namespaces[i]]
+	        if (!locale) {
+	          ret = key
+	          break
+	        } else {
+	          ret = locale
+	        }
+	      }
+	    }
+	    return ret
+	  }
 
-require.modules = {};
+	  Vue.directive('t', {
+	    isLiteral: true,
+	    bind: function () {
+	      if (this.el.nodeType !== 1) { return }
 
-/**
- * Register module at `name` with callback `definition`.
- *
- * @param {String} name
- * @param {Function} definition
- * @api private
- */
+	      this.el.textContent = Vue.t(this.expression)
+	    }
+	  })
+	}
 
-require.register = function (name, definition) {
-  require.modules[name] = {
-    definition: definition
-  };
-};
 
-/**
- * Define a module's exports immediately with `exports`.
- *
- * @param {String} name
- * @param {Generic} exports
- * @api private
- */
-
-require.define = function (name, exports) {
-  require.modules[name] = {
-    exports: exports
-  };
-};
-require.register("vue-i18n", function (exports, module) {
-/**
- * Expose internationalization plugin
- *
- * @param {Object} Vue
- * @param {Object} opts
- */
-
-module.exports = function (Vue, opts) {
-  opts = opts || {}
-  var lang = opts.lang || 'en'
-  var locales = opts.locales || opts.resources || {}
-
-  Vue.t = function (key) {
-    var ret = key || ''
-    var locale = locales[lang]
-    if (key && locale) {
-      var namespaces = key.split('.')
-      for (var i = 0; i < namespaces.length; i++) {
-        locale = locale[namespaces[i]]
-        if (!locale) {
-          ret = key
-          break
-        } else {
-          ret = locale
-        }
-      }
-    }
-    return ret
-  }
-
-  Vue.directive('t', {
-    isLiteral: true,
-    bind: function () {
-      if (this.el.nodeType !== 1) { return }
-
-      this.el.textContent = Vue.t(this.expression)
-    }
-  })
-}
-
+/***/ }
+/******/ ])
 });
-
-if (typeof exports == "object") {
-  module.exports = require("vue-i18n");
-} else if (typeof define == "function" && define.amd) {
-  define([], function(){ return require("vue-i18n"); });
-} else {
-  this["vue-i18n"] = require("vue-i18n");
-}
-})()
