8314d46c627ebb536b64390a3ec735f61fee2a4e
kazuya kawaguchi <kawakazu80@gmail.com>
2016-04-10 02:56:29 +0900

    add utility module :heavy_plus_sign:

---
+++ src/util.js
@@ -0,0 +1,25 @@
+/**
+ * Utilties
+ */
+
+// export default for holding the Vue reference
+const exports = {}
+export default exports
+
+
+/**
+ * warn
+ *
+ * @param {String} msg
+ * @param {Error} [err]
+ *
+ */
+
+export function warn (msg, err) {
+  if (window.console) {
+    console.warn('[vue-i18n] ' + msg)
+    if (err) {
+      console.warn(err.stack)
+    }
+  }
+}
