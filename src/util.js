f7d597ac9b0d768c0f6b8936ef668174635d7515
kazuya kawaguchi <kawakazu80@gmail.com>
2016-04-13 07:31:19 +0900

    chore: add utilities

--- src/util.js
+++ src/util.js
@@ -23,3 +23,35 @@ export function warn (msg, err) {
     }
   }
 }
+
+/**
+ * getWatcher
+ *
+ * @param {Vue} vm
+ * @return {Watcher}
+ */
+
+let Watcher
+export function getWatcher (vm) {
+  if (!Watcher) {
+    const unwatch = vm.$watch('__watcher__', (a) => {})
+    Watcher = vm._watchers[0].constructor
+    unwatch()
+  }
+  return Watcher
+}
+
+/**
+ * getDep
+ *
+ * @param {Vue} vm
+ * @return {Dep}
+ */
+
+let Dep
+export function getDep (vm) {
+  if (!Dep) {
+    Dep = vm._data.__ob__.dep.constructor
+  }
+  return Dep
+}
