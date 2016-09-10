bea396a64b5db9b577f9ba3c47d681c6a2124121
kazuya kawaguchi <kawakazu80@gmail.com>
2016-09-10 23:54:46 +0900

    :wrench: config(build): update gitbook installation cdn generation

--- config/build.js
+++ config/build.js
@@ -20,6 +20,10 @@ var installation = fs
     /<script src="https:\/\/cdn\.jsdelivr\.net\/vue\.i18n\/[\d\.]+.[\d]+\/vue-i18n\.min\.js"><\/script>/,
     '<script src="https://cdn.jsdelivr.net/vue.i18n/' + pack.version + '/vue-i18n.min.js"></script>'
   )
+  .replace(
+    /<script src="https:\/\/unpkg\.com\/vue-i18n@[\d\.]+.[\d]+\/dist\/vue-i18n\.min\.js"><\/script>/,
+    '<script src="https://unpkg.com/vue-i18n@' + pack.version + '/dist/vue-i18n.min.js"></script>'
+  )
 fs.writeFileSync('./gitbook/installation.md', installation)
 
 
