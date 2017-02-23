f8d0f3c4a59e5f8c153913796ec51ff00cb1c97f
kazuya kawaguchi <kawakazu80@gmail.com>
2017-02-23 12:54:24 +0900

    :pencil: docs(examples): update examples

--- test/e2e/test/component.js
+++ test/e2e/test/component.js
@@ -4,7 +4,7 @@ module.exports = {
       .url('http://localhost:8080/examples/component/')
       .waitForElementVisible('#app', 1000)
       .assert.containsText('p:nth-child(1)', 'こんにちは、世界')
-      .assert.containsText('p:nth-child(2)', 'component1 local: こんにちは、component1')
+      .assert.containsText('p:nth-child(2)', 'component1 local: hello component1')
       .end()
   }
 }
