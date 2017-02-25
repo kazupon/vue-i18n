50e237038d469814b9e747eb380dae3bb9c05d01
kazuya kawaguchi <kawakazu80@gmail.com>
2017-02-25 10:45:19 +0900

    :shirt: refactor(asset): tweak merge strategy

--- src/asset.js
+++ src/asset.js
@@ -2,14 +2,14 @@ export default function (Vue) {
   const strats = Vue.config.optionMergeStrategies
   if (strats) {
     strats.i18n = (parent, child) => {
+      const ret = Object.create(null)
       if (!child) { return parent }
       if (!parent) { return child }
       if (!child & !parent) {
         // TODO: should be warn
-        return {}
+        return ret
       }
-      const ret = Object.create(null)
-      Vue.extend(ret, parent)
+      Vue.util.extend(ret, parent)
       for (const key in child) {
         ret[key] = child[key]
       }
