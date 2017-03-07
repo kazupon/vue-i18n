0e4ac39f33104804364b1027ea27bffc9cd741f8
kazuya kawaguchi <kawakazu80@gmail.com>
2017-03-08 00:24:16 +0900

    :zap: improvement(mixin): add error throwings and a warning

--- src/mixin.js
+++ src/mixin.js
@@ -1,11 +1,15 @@
 /* @flow */
 
 import VueI18n from './index'
+import { typeName, isPlainObject, warn } from './util'
 
 export default {
   computed: {
     $t () {
-      // HACK: add dependency tracking !!
+      if (!this.$i18n) {
+        throw Error(`Failed in $t due to not find VueI18n instance`)
+      }
+      // add dependency tracking !!
       const locale: string = this.$i18n.locale
       const messages: Messages = this.$i18n.messages
       return (key: string, ...args: any): string => {
@@ -14,7 +18,10 @@ export default {
     },
 
     $tc () {
-      // HACK: add dependency tracking !!
+      if (!this.$i18n) {
+        throw Error(`Failed in $tc due to not find VueI18n instance`)
+      }
+      // add dependency tracking !!
       const locale: string = this.$i18n.locale
       const messages: Messages = this.$i18n.messages
       return (key: string, choice?: number, ...args: any): string => {
@@ -23,7 +30,10 @@ export default {
     },
 
     $te () {
-      // HACK: add dependency tracking !!
+      if (!this.$i18n) {
+        throw Error(`Failed in $te due to not find VueI18n instance`)
+      }
+      // add dependency tracking !!
       const locale: string = this.$i18n.locale
       const messages: Messages = this.$i18n.messages
       return (key: string, ...args: any): boolean => {
@@ -35,22 +45,26 @@ export default {
   beforeCreate () {
     const options: any = this.$options
     if (options.i18n) {
-      if (options.i18n instanceof VueI18n) {
+      if (typeName(options.i18n) === 'VueI18n') {
         this.$i18n = options.i18n
-      } else {
+      } else if (isPlainObject(options.i18n)) {
         // component local i18n
-        if (this.$root && this.$root.$i18n && this.$root.$i18n instanceof VueI18n) {
+        if (this.$root && this.$root.$i18n && typeName(this.$root.$i18n) === 'VueI18n') {
           options.i18n.root = this.$root.$i18n
         }
         this.$i18n = new VueI18n(options.i18n)
+      } else {
+        if (process.env.NODE_ENV !== 'production') {
+          warn(`Cannot be interpreted 'i18n' options.`)
+        }
       }
-    } else if (this.$root && this.$root.$i18n && this.$root.$i18n instanceof VueI18n) {
+    } else if (this.$root && this.$root.$i18n && typeName(this.$root.$i18n) === 'VueI18n') {
       // root i18n
       this.$i18n = this.$root.$i18n
     }
   },
 
-  beforeDestroy () {
+  destroyed () {
     this.$i18n = null
   }
 }
