a6b7e374bda4478a3ba0724ba45489047f9b73b4
kazuya kawaguchi <kawakazu80@gmail.com>
2017-03-18 00:22:57 +0900

    :bug: bug(mixin): fix computed props errors

--- src/mixin.js
+++ src/mixin.js
@@ -3,56 +3,52 @@
 import VueI18n from './index'
 import { isPlainObject, warn } from './util'
 
+const $t = (vm: any): Function => {
+  // add dependency tracking !!
+  const locale: Locale = vm.$i18n.locale
+  const messages: LocaleMessages = vm.$i18n.messages
+  return (key: string, ...args: any): TranslateResult => {
+    return vm.$i18n._t(key, locale, messages, vm, ...args)
+  }
+}
+const $tc = (vm: any): Function => {
+  // add dependency tracking !!
+  const locale: Locale = vm.$i18n.locale
+  const messages: LocaleMessages = vm.$i18n.messages
+  return (key: string, choice?: number, ...args: any): TranslateResult => {
+    return vm.$i18n._tc(key, locale, messages, vm, choice, ...args)
+  }
+}
+const $te = (vm: any): Function => {
+  // add dependency tracking !!
+  const locale: Locale = vm.$i18n.locale
+  const messages: LocaleMessages = vm.$i18n.messages
+  return (key: string, ...args: any): boolean => {
+    return vm.$i18n._te(key, locale, messages, ...args)
+  }
+}
+
+function defineComputed (vm: any, options: any): void {
+  options.computed = options.computed || {}
+  options.computed.$t = () => $t(vm)
+  options.computed.$tc = () => $tc(vm)
+  options.computed.$te = () => $te(vm)
+}
+
 export default {
-  computed: {
-    $t (): Function {
-      if (!this.$i18n) {
-        throw Error(`Failed in $t due to not find VueI18n instance`)
-      }
-      // add dependency tracking !!
-      const locale: Locale = this.$i18n.locale
-      const messages: LocaleMessages = this.$i18n.messages
-      return (key: string, ...args: any): TranslateResult => {
-        return this.$i18n._t(key, locale, messages, this, ...args)
-      }
-    },
-
-    $tc (): Function {
-      if (!this.$i18n) {
-        throw Error(`Failed in $tc due to not find VueI18n instance`)
-      }
-      // add dependency tracking !!
-      const locale: Locale = this.$i18n.locale
-      const messages: LocaleMessages = this.$i18n.messages
-      return (key: string, choice?: number, ...args: any): TranslateResult => {
-        return this.$i18n._tc(key, locale, messages, this, choice, ...args)
-      }
-    },
-
-    $te (): Function {
-      if (!this.$i18n) {
-        throw Error(`Failed in $te due to not find VueI18n instance`)
-      }
-      // add dependency tracking !!
-      const locale: Locale = this.$i18n.locale
-      const messages: LocaleMessages = this.$i18n.messages
-      return (key: string, ...args: any): boolean => {
-        return this.$i18n._te(key, locale, messages, ...args)
-      }
-    }
-  },
-
   beforeCreate (): void {
     const options: any = this.$options
     if (options.i18n) {
       if (options.i18n instanceof VueI18n) {
         this.$i18n = options.i18n
+        defineComputed(this, options)
       } else if (isPlainObject(options.i18n)) {
         // component local i18n
         if (this.$root && this.$root.$i18n && this.$root.$i18n instanceof VueI18n) {
           options.i18n.root = this.$root.$i18n
         }
         this.$i18n = new VueI18n(options.i18n)
+        defineComputed(this, options)
         if (options.i18n.sync) {
           this._localeWatcher = this.$i18n.watchLocale()
         }
@@ -64,10 +60,13 @@ export default {
     } else if (this.$root && this.$root.$i18n && this.$root.$i18n instanceof VueI18n) {
       // root i18n
       this.$i18n = this.$root.$i18n
+      defineComputed(this, options)
     }
   },
 
-  destroyed (): void {
+  beforeDestroy (): void {
+    if (!this.$i18n) { return }
+
     if (this._localeWatcher) {
       this.$i18n.unwatchLocale()
       delete this._localeWatcher
