75772ef763557a91316ec320b2adfae4260a8d82
kazuya kawaguchi <kawakazu80@gmail.com>
2017-03-20 18:59:28 +0900

    :green_heart: test(e2e): fix component example

--- test/e2e/test/component.js
+++ test/e2e/test/component.js
@@ -4,7 +4,8 @@ module.exports = {
       .url('http://localhost:8080/examples/component/')
       .waitForElementVisible('#app', 1000)
       .assert.containsText('p:nth-child(1)', 'こんにちは、世界')
-      .assert.containsText('p:nth-child(2)', 'component1 local: hello component1')
+      .assert.containsText('div.container p:nth-child(1)', 'Component1 locale messages: こんにちは、component1')
+      .assert.containsText('div.container p:nth-child(2)', 'Fallback global locale messages: おはよう、世界！')
       .end()
   }
 }
