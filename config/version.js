93a768e2d4539b9f84a5bbd8437b7e5d567cde9f
kazuya kawaguchi <kawakazu80@gmail.com>
2017-03-22 11:20:54 +0900

    :wrench: config(version): update versioning

--- config/version.js
+++ config/version.js
@@ -5,11 +5,7 @@ const pack = require('../package.json')
 const installation = fs
   .readFileSync('./gitbook/en/installation.md', 'utf-8')
   .replace(
-    /<script src="https:\/\/cdn\.jsdelivr\.net\/vue\.i18n\/[\d.]+.[\d]+\/vue-i18n\.min\.js"><\/script>/,
-    '<script src="https://cdn.jsdelivr.net/vue.i18n/' + pack.version + '/vue-i18n.min.js"></script>'
-  )
-  .replace(
-    /<script src="https:\/\/unpkg\.com\/vue-i18n@[\d.]+.[\d]+\/dist\/vue-i18n\.min\.js"><\/script>/,
-    '<script src="https://unpkg.com/vue-i18n@' + pack.version + '/dist/vue-i18n.min.js"></script>'
+    /https:\/\/unpkg\.com\/vue-i18n@[\d.]+.[\d]+\/dist\/vue-i18n\.js/,
+    'https://unpkg.com/vue-i18n@' + pack.version + '/dist/vue-i18n.js.'
   )
 fs.writeFileSync('./gitbook/en/installation.md', installation)
