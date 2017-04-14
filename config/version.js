dceae380be27c75746d75d6a410a14393f108ead
kazuya kawaguchi <kawakazu80@gmail.com>
2017-04-15 08:25:49 +0900

    :wrench: config: tweak version

--- config/version.js
+++ config/version.js
@@ -6,6 +6,6 @@ const installation = fs
   .readFileSync('./gitbook/en/installation.md', 'utf-8')
   .replace(
     /https:\/\/unpkg\.com\/vue-i18n@[\d.]+.[\d]+\/dist\/vue-i18n\.js/,
-    'https://unpkg.com/vue-i18n@' + pack.version + '/dist/vue-i18n.js.'
+    'https://unpkg.com/vue-i18n@' + pack.version + '/dist/vue-i18n.js'
   )
 fs.writeFileSync('./gitbook/en/installation.md', installation)
