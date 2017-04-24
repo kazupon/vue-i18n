efe28c14d00391efa71ccc5bf1efc2f9464bb866
kazuya kawaguchi <kawakazu80@gmail.com>
2017-04-25 00:41:11 +0900

    :green_heart: test(CI): fix more timezone errors

--- test/unit/fixture/datetime.js
+++ test/unit/fixture/datetime.js
@@ -1,19 +1,27 @@
-export default {
+const formats = {
   'en-US': {
     short: { // DD/MM/YYYY, hh:mm (AM|PM)
       year: 'numeric', month: '2-digit', day: '2-digit',
-      hour: '2-digit', minute: '2-digit', timeZone: 'America/New_York'
+      hour: '2-digit', minute: '2-digit'
     }
   },
   'ja-JP': {
     long: { // YYYY/MM/DD hh:mm:ss
       year: 'numeric', month: '2-digit', day: '2-digit',
-      hour: '2-digit', minute: '2-digit', second: '2-digit',
-      timeZone: 'Asia/Tokyo'
+      hour: '2-digit', minute: '2-digit', second: '2-digit'
     },
     short: { // YYYY/MM/DD hh:mm
       year: 'numeric', month: '2-digit', day: '2-digit',
-      hour: '2-digit', minute: '2-digit', timeZone: 'Asia/Tokyo'
+      hour: '2-digit', minute: '2-digit'
     }
   }
 }
+
+// NOTE: avoid webkit(phatomjs/safari) & Intl timeZone don't support...
+if (!isWebkit) {
+  formats['en-US']['short']['timeZone'] = 'America/New_York'
+  formats['ja-JP']['short']['timeZone'] = 'Asia/Tokyo'
+  formats['ja-JP']['long']['timeZone'] = 'Asia/Tokyo'
+}
+
+export default formats
