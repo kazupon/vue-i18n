4c196735b774b5ae884746ff660284fd8a8ed499
kazuya kawaguchi <kawakazu80@gmail.com>
2017-05-12 00:41:22 +0900

    :up: update(typescript): add typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -11,18 +11,59 @@ declare namespace VueI18n {
   interface LocaleMessageArray { [index: number]: LocaleMessage; }
   interface LocaleMessages { [key: string]: LocaleMessageObject; }
   type TranslateResult = string | LocaleMessageArray;
+  interface DateTimeFormatOptions {
+    year?: string;
+    month?: string;
+    day?: string;
+    hour?: string;
+    minute?: string;
+    second?: string;
+    weekday?: string;
+    hour12?: boolean;
+    era?: string;
+    timeZone?: string;
+    timeZoneName?: string;
+    localeMatcher?: string;
+    formatMatcher?: string;
+  }
+  interface DateTimeFormat { [key: string]: DateTimeFormatOptions; }
+  interface DateTimeFormats { [key: string]: DateTimeFormat; }
+  type DateTimeFormatResult = string;
+  interface NumberFormatOptions {
+    style?: string;
+    currency?: string;
+    currencyDisplay?: string;
+    useGrouping?: boolean;
+    minimumIntegerDigits?: number;
+    minimumFractionDigits?: number;
+    maximumFractionDigits?: number;
+    minimumSignificantDigits?: number;
+    maximumSignificantDigits?: number;
+    localeMatcher?: string;
+    formatMatcher?: string;
+  }
+  interface NumberFormat { [key: string]: NumberFormatOptions; }
+  interface NumberFormats { [key: string]: NumberFormat; }
+  type NumberFormatResult = string;
 
   interface Formatter {
-    format(message: string, values: Values): string;
+    format(message: string, values: Values): any[];
   }
 
   type MissingHandler = (locale: Locale, key: Path, vm?: Vue) => void;
+
+  interface IntlAvailability {
+    dateTimeFormat: boolean;
+    numberFormat: boolean;
+  }
 
   // tslint:disable-next-line:interface-name
   interface I18nOptions {
     locale?: Locale;
     fallbackLocale?: Locale;
     messages?: LocaleMessages;
+    dateTimeFormats?: DateTimeFormats;
+    numberFormats?: NumberFormats;
     formatter?: Formatter;
     missing?: MissingHandler;
     fallbackRoot?: boolean;
@@ -35,6 +76,8 @@ declare class VueI18n {
   constructor(options?: VueI18n.I18nOptions)
 
   readonly messages: VueI18n.LocaleMessages;
+  readonly dateTimeFormats: VueI18n.DateTimeFormats;
+  readonly numberFormats: VueI18n.NumberFormats;
 
   locale: VueI18n.Locale;
   fallbackLocale: VueI18n.Locale;
@@ -47,13 +90,26 @@ declare class VueI18n {
   tc(key: VueI18n.Path, choice?: VueI18n.Choice, values?: VueI18n.Values): string;
   tc(key: VueI18n.Path, choice: VueI18n.Choice, locale: VueI18n.Locale, values?: VueI18n.Values): string;
   te(key: VueI18n.Path, locale?: VueI18n.Locale): boolean;
+  d(value: number | Date, key?: VueI18n.Path, locale?: VueI18n.Locale): VueI18n.DateTimeFormatResult;
+  d(value: number | Date, args?: { [key: string]: string }): VueI18n.DateTimeFormatResult;
+  n(value: number, key?: VueI18n.Path, locale?: VueI18n.Locale): VueI18n.NumberFormatResult;
+  n(value: number, args?: { [key: string]: string }): VueI18n.NumberFormatResult;
 
   getLocaleMessage(locale: VueI18n.Locale): VueI18n.LocaleMessageObject;
   setLocaleMessage(locale: VueI18n.Locale, message: VueI18n.LocaleMessageObject): void;
   mergeLocaleMessage(locale: VueI18n.Locale, message: VueI18n.LocaleMessageObject): void;
 
+  getDateTimeFormat(locale: VueI18n.Locale): VueI18n.DateTimeFormat;
+  setDateTimeFormat(locale: VueI18n.Locale, format: VueI18n.DateTimeFormat): void;
+  mergeDateTimeFormat(locale: VueI18n.Locale, format: VueI18n.DateTimeFormat): void;
+
+  getNumberFormat(locale: VueI18n.Locale): VueI18n.NumberFormat;
+  setNumberFormat(locale: VueI18n.Locale, format: VueI18n.NumberFormat): void;
+  mergeNumberFormat(locale: VueI18n.Locale, format: VueI18n.NumberFormat): void;
+
   static install: PluginFunction<never>;
   static version: string;
+  static availabilities: VueI18n.IntlAvailability;
 }
 
 declare module 'vue/types/vue' {
@@ -62,6 +118,8 @@ declare module 'vue/types/vue' {
     $t: typeof VueI18n.prototype.t;
     $tc: typeof VueI18n.prototype.tc;
     $te: typeof VueI18n.prototype.te;
+    $d: typeof VueI18n.prototype.d;
+    $n: typeof VueI18n.prototype.n;
   }
 }
 
--- types/test.ts
+++ types/test.ts
@@ -14,22 +14,45 @@ VueI18n.install(Vue);
 VueI18n.version;                    // $ExpectType string
 
 /**
+ * VueI18n.availabilities
+ */
+VueI18n.availabilities;             // $ExpectType IntlAvailability
+
+/**
  * VueI18n Instance
  */
 const locale = 'locale';
 const key = 'key';
 const value = 'value';
+const dateTimeFormatOptions = {
+  year: '2-digit',
+  timeZone: 'Asia/Tokyo'
+};
+const numberFormatOptions = {
+  style: 'currency',
+  currency: 'JPY'
+};
 const i18n = new VueI18n({
   locale,
   fallbackLocale: locale,
   messages: {
     [locale]: {
-      [key]: value,
-    },
+      [key]: value
+    }
+  },
+  dateTimeFormats: {
+    [locale]: {
+      [key]: dateTimeFormatOptions
+    }
+  },
+  numberFormats: {
+    [locale]: {
+      [key]: numberFormatOptions
+    }
   },
   formatter: {
     format(message, values) {
-      return message;
+      return [message];
     },
   },
   missing(locale, key, vm) {
@@ -39,6 +62,8 @@ const i18n = new VueI18n({
   silentTranslationWarn: true,
 });
 i18n.messages[locale][key];         // $ExpectType LocaleMessage
+i18n.dateTimeFormats[locale][key];  // $ExpectType DateTimeFormatOptions
+i18n.numberFormats[locale][key];    // $ExpectType NumberFormatOptions
 i18n.locale;                        // $ExpectType string
 i18n.fallbackLocale;                // $ExpectType string
 i18n.missing;                       // $ExpectType MissingHandler
@@ -47,6 +72,12 @@ i18n.silentTranslationWarn;         // $ExpectType boolean
 i18n.setLocaleMessage;              // $ExpectType (locale: string, message: LocaleMessageObject) => void
 i18n.getLocaleMessage;              // $ExpectType (locale: string) => LocaleMessageObject
 i18n.mergeLocaleMessage;            // $ExpectType (locale: string, message: LocaleMessageObject) => void
+i18n.setDateTimeFormat;             // $ExpectType (locale: string, format: DateTimeFormat) => void
+i18n.getDateTimeFormat;             // $ExpectType (locale: string) => DateTimeFormat
+i18n.mergeDateTimeFormat;           // $ExpectType (locale: string, format: DateTimeFormat) => void
+i18n.setNumberFormat;               // $ExpectType (locale: string, format: NumberFormat) => void
+i18n.getNumberFormat;             // $ExpectType (locale: string) => NumberFormat
+i18n.mergeNumberFormat;           // $ExpectType (locale: string, format: NumberFormat) => void
 // $ExpectType { (key: string, values?: { [key: string]: any; } | undefined): TranslateResult; (key: string, locale: string, values?: { [key: string]: any; } | undefined): TranslateResult; }
 i18n.t;
 // tslint:disable-next-line:max-line-length
@@ -54,6 +85,12 @@ i18n.t;
 i18n.tc;
 // $ExpectType (key: string, locale?: string | undefined) => boolean
 i18n.te;
+// tslint:disable-next-line:max-line-length
+// $ExpectType { (value: number | Date, key?: string | undefined, locale?: string | undefined): string; (value: number | Date, args?: { [key: string]: string; } | undefined): string; }
+i18n.d;
+// tslint:disable-next-line:max-line-length
+// $ExpectType { (value: number, key?: string | undefined, locale?: string | undefined): string; (value: number, args?: { [key: string]: string; } | undefined): string; }
+i18n.n;
 
 /**
  * Vue
@@ -77,6 +114,12 @@ vm.$tc(key, 1, locale, []);         // $ExpectType string
 vm.$tc(key, 1, locale, {});         // $ExpectType string
 vm.$te(key);                        // $ExpectType boolean
 vm.$te(key, locale);                // $ExpectType boolean
+vm.$d(1, key);                      // $ExpectType string
+vm.$d(1, key, locale);              // $ExpectType string
+vm.$d(new Date(), { key, locale }); // $ExpectType string
+vm.$n(1, key);                      // $ExpectType string
+vm.$n(1, key, locale);              // $ExpectType string
+vm.$n(100, { key, locale });        // $ExpectType string
 
 /**
  * VueI18n
@@ -91,6 +134,12 @@ vm.$te(key, locale);                // $ExpectType boolean
   let localeMessageArray: VueI18n.LocaleMessageArray;
   let localeMessages: VueI18n.LocaleMessages;
   let translateResult: VueI18n.TranslateResult;
+  let dateTimeFormatOptions: VueI18n.DateTimeFormatOptions;
+  let dateTimeFormat: VueI18n.DateTimeFormat;
+  let dateTimeFormats: VueI18n.DateTimeFormats;
+  let numberFormatOptions: VueI18n.NumberFormatOptions;
+  let NumberFormat: VueI18n.NumberFormat;
+  let numberFormats: VueI18n.NumberFormats;
   let formatter: VueI18n.Formatter;
   let missingHandler: VueI18n.MissingHandler;
   let i18nOptions: VueI18n.I18nOptions;
