d5c9b6180ac320a8ec260b46305180c5f67b609e
Leo Deng <myst.dg@gmail.com>
2017-08-27 11:52:17 +0800

    feature(component): place and places prop for component interpolation

--- src/component.js
+++ src/component.js
@@ -16,10 +16,18 @@ export default {
     },
     locale: {
       type: String
+    },
+    places: {
+      type: [Array, Object]
     }
   },
   render (h: Function, { props, data, children, parent }: Object) {
     const i18n = parent.$i18n
+
+    children = (children || []).filter(child => {
+      return child.tag || (child.text = child.text.trim())
+    })
+
     if (!i18n) {
       if (process.env.NODE_ENV !== 'production') {
         warn('Cannot find VueI18n instance!')
@@ -30,14 +38,41 @@ export default {
     const path: Path = props.path
     const locale: ?Locale = props.locale
 
-    const params: Array<any> = []
-    locale && params.push(locale)
-    children.forEach(child => {
-      if (child.tag || child.text.trim()) {
-        params.push(child)
+    const params: Object = {}
+    const places: Array<any> | Object = props.places || {}
+
+    const hasPlaces: boolean = Array.isArray(places)
+      ? places.length > 0
+      : Object.keys(places).length > 0
+
+    const everyPlace: boolean = children.every(child => {
+      if (child.data && child.data.attrs) {
+        const place = child.data.attrs.place
+        return (typeof place !== 'undefined') && place !== ''
       }
     })
 
-    return h(props.tag, data, i18n.i(path, ...params))
+    if (hasPlaces && children.length > 0 && !everyPlace) {
+      warn('If places prop is set, all child elements must have place prop set.')
+    }
+
+    if (Array.isArray(places)) {
+      places.forEach((el, i) => {
+        params[i] = el
+      })
+    } else {
+      Object.keys(places).forEach(key => {
+        params[key] = places[key]
+      })
+    }
+
+    children.forEach((child, i: number) => {
+      const key: string = everyPlace
+        ? `${child.data.attrs.place}`
+        : `${i}`
+      params[key] = child
+    })
+
+    return h(props.tag, data, i18n.i(path, locale, params))
   }
 }
