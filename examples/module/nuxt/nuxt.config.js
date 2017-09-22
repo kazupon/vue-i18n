8a97c1ac31091c2db5e99c83bb574b36d7df2a40
kazuya kawaguchi <kawakazu80@gmail.com>
2017-09-22 16:20:56 +0900

    :lollipop: examples(module): add compiler module for nuxt.js

---
+++ examples/module/nuxt/nuxt.config.js
@@ -0,0 +1,50 @@
+const Vue = require('vue')
+const VueI18n = require('vue-i18n')
+const i18nExtensions = require('vue-i18n-extensions')
+
+Vue.use(VueI18n)
+
+const i18n = new VueI18n({
+  locale: 'ja',
+  messages: {
+    en: require('./locales/en.json'),
+    ja: require('./locales/ja.json')
+  }
+})
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'nuxt',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, ctx) {
+      // inject `compilerModules` to vue-loader options
+      config.module.rules.forEach(rule => {
+        if (rule.loader === 'vue-loader') {
+          rule.options.compilerModules = i18nExtensions.module(i18n)
+        }
+      })
+    }
+  }
+}
---
+++ examples/module/nuxt/pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title" v-t="'title.main'"></h1>
+      <h2 class="subtitle" v-t="'title.sub'"></h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green" v-t="'links.docs'"></a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey" v-t="'links.github'"></a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
