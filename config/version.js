4af711b8d4a65115fbb3fa9eec1476b965d5e413
kazuya kawaguchi <kawakazu80@gmail.com>
2018-05-30 17:18:01 +0900

    :wrench: config(version): update auto doc versioning

--- config/version.js
+++ config/version.js
@@ -3,9 +3,9 @@ const pack = require('../package.json')
 
 // update installation.md
 const installation = fs
-  .readFileSync('./vuepress/current/en/installation.md', 'utf-8')
+  .readFileSync('./vuepress/installation.md', 'utf-8')
   .replace(
     /https:\/\/unpkg\.com\/vue-i18n@[\d.]+.[\d]+\/dist\/vue-i18n\.js/,
     'https://unpkg.com/vue-i18n@' + pack.version + '/dist/vue-i18n.js'
   )
-fs.writeFileSync('./vuepress/current/en/installation.md', installation)
+fs.writeFileSync('./vuepress/installation.md', installation)
