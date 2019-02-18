7594504e03eab31d2135c23bea92c8839dfde9b7
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-02-18 12:22:01 +0100

    update sfc example, closes #427

--- examples/sfc/src/main.js
+++ examples/sfc/src/main.js
@@ -1,19 +1,18 @@
 import Vue from 'vue'
-import VueI18n from 'vue-i18n'
 import App from './App.vue'
+import VueI18n from 'vue-i18n'
 
+Vue.config.productionTip = false
 Vue.use(VueI18n)
 
 const i18n = new VueI18n({
   locale: 'en',
   messages: {
-    en: {
-    }
-  }
+    en: {},
+  },
 })
 
 new Vue({
   i18n,
-  el: '#app',
-  render: h => h(App)
-})
+  render: h => h(App),
+}).$mount('#app')
