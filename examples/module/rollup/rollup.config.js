06633e471531fafef51838191df287b8eaf04b9d
kazuya kawaguchi <kawakazu80@gmail.com>
2019-06-11 03:44:51 +0900

    :lollipop: examples(module/rollup): updates

--- examples/module/rollup/rollup.config.js
+++ examples/module/rollup/rollup.config.js
@@ -1,8 +1,10 @@
 import vue from 'rollup-plugin-vue'
-import babel from 'rollup-plugin-buble'
-import replace from 'rollup-plugin-replace'
+import buble from 'rollup-plugin-buble'
 import resolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
 import commonjs from 'rollup-plugin-commonjs'
+import { uglify } from 'rollup-plugin-uglify'
+import serve from 'rollup-plugin-serve'
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import { module } from 'vue-i18n-extensions'
@@ -33,28 +35,54 @@ const i18n = new VueI18n({
   }
 })
 
-export default {
-  moduleName: 'App',
-  entry: 'src/index.js',
-  dest: 'dist/bundle.js',
-  format: 'iife',
-  plugins: [
-    resolve({
-      jsnext: true,
-      main: true,
-      browser: true
-    }),
-    commonjs(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    vue({
-      css: 'dist/bundle.css',
-      compileTemplate: true,
-      compileOptions: {
+const external = [
+  //'vue'
+]
+const globals = {
+  //vue: 'Vue'
+}
+
+const plugins = [
+  resolve({
+    customResolveOptions: {
+      moduleDirectory: 'node_modules'
+    }
+  }),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  }),
+  commonjs(),
+  vue({
+    css: true,
+    compileTemplate: true,
+    template: {
+      compilerOptions: {
         modules: [module(i18n)]
       }
-    }),
-    babel()
-  ]
+    }
+  }),
+  buble({ exclude: 'node_modules/**' })
+]
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(uglify())
+} else if (process.env.NODE_ENV === 'development') {
+  plugins.push(serve({
+    contentBase: ['dist'],
+    port: 5000,
+    open: true
+  }))
+}
+
+export default {
+  input: 'src/index.js',
+  external,
+  output: {
+    file: 'dist/bundle.js',
+    name: 'app',
+    format: 'iife',
+    sourcemap: true,
+    globals
+  },
+  plugins
 }
