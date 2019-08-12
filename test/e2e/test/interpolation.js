ec30c93738f9be38e0bcc63038668657f7b44e49
kazuya kawaguchi <kawakazu80@gmail.com>
2019-08-12 19:10:19 +0900

    :lollipop: examples(interpolation): add slots usage example

--- test/e2e/test/interpolation.js
+++ test/e2e/test/interpolation.js
@@ -1,7 +1,7 @@
 module.exports = {
   interpolation: function (browser) {
     browser
-      .url('http://localhost:8080/examples/interpolation/')
+      .url('http://localhost:8080/examples/interpolation/slots')
       .waitForElementVisible('#app', 1000)
       .assert.containsText('label[for="tos"]', 'I accept xxx Term of Service.')
       .end()
