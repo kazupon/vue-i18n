ab08d1d5d30f4f59a4e68cc0f6ad5eee4c567c21
Ivan Demchuk <ivan.demchuk@gmail.com>
2019-09-06 13:25:28 +0300

    Filter after checking for null

--- src/components/interpolation.js
+++ src/components/interpolation.js
@@ -54,13 +54,13 @@ function onlyHasDefaultPlace (params) {
 
 function useLegacyPlaces (children, places) {
   const params = places ? createParamsFromPlaces(places) : {}
+  
+  if (!children) { return params }
 
   // Filter empty text nodes
-  children = (children || []).filter(child => {
+  children = children.filter(child => {
     return child.tag || (child.text = child.text.trim())
   })
-
-  if (!children) { return params }
 
   const everyPlace = children.every(vnodeHasPlaceAttribute)
   if (process.env.NODE_ENV !== 'production' && everyPlace) {
