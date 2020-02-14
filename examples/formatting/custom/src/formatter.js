a0fc5c2201ab351df813f630e41b94715a9387c1
Erick Wilder <erickwilder@gmail.com>
2020-02-14 07:47:18 +0100

    feat(examples/formatting): update custom formatter example (#781)
    * feat(examples/formatting): update custom formatter example
    
    Use the new `messageformat` interface[1] that changes the data type from compiled messageformat
    functions to arrays, instead of the old behavior that converted everything to strings. The old
    behaviour of `messageformat` could not be used without some string manipulation hack to make
    component interpolation to work.
    
    This also takes into consideration the simplified interpolation, slot-based interpolation, and
    the _deprecated_ `place`/`places` interpolation styles.
    
    **Other changes**
    
    * Update dependencies; there was a bug in the vue-i18n version that prevented slot-based interpolation
      to correctly pass `values` to custom formatters.
    * Update all vue-related dependencies to remove compromised packages reported by `npm audit`
    
    [1]  https://github.com/messageformat/messageformat/pull/242
    
    * fmt(examples/formatting): remove unused function param from custom formatter

--- examples/formatting/custom/src/formatter.js
+++ examples/formatting/custom/src/formatter.js
@@ -3,7 +3,7 @@ import MessageFormat from 'messageformat'
 export default class CustomFormatter {
   constructor (options = {}) {
     this._locale = options.locale || 'en-US'
-    this._formatter = new MessageFormat(this._locale)
+    this._formatter = new MessageFormat(this._locale, {returnType: 'values'})
     this._caches = Object.create(null)
   }
 
@@ -13,6 +13,54 @@ export default class CustomFormatter {
       fn = this._formatter.compile(message, this._locale)
       this._caches[message] = fn
     }
-    return [fn(values)]
+
+    if (values !== null && values !== undefined) {
+      /**
+       * Component interpolation in the following format passes `values` as an Array-like `Object`
+       *
+       * ```vue
+       * <i18n path="terms" for="tos">
+       *   I agree with the <a href="/terms">{{ $t('tos') }}</a>
+       * </i18n>
+       * ```
+       *
+       * VNodes _may_ be found when using interpolated values with either the deprecated `places` **or** the new slot
+       * syntax
+       */
+      const valuesEntries = Object.entries(values)
+      const isNonSlotInterpolation = valuesEntries.some(entry => {
+        const [key, value] = entry
+        return value.hasOwnProperty('componentOptions') && isNaN(Number.parseFloat(key)) === false
+      })
+
+      if (isNonSlotInterpolation) {
+        /**
+         * In this case, we iterate over the VNode list and preserve **only** VNodes with tags and return them, wrapped
+         * in a list.
+         *
+         * Rationale: If text nodes are kept, the positional lookup from vue-i18n will fail, rendering the first
+         * child again (i.e `I agree with the, I agree with the`)
+         */
+        values = valuesEntries.map(entry => {
+          const [, vnode] = entry
+          if (vnode.tag !== undefined) {
+            return [vnode]
+          }
+        }).filter(Boolean)
+      }
+    }
+    const result = fn(values).map(piece => {
+      if (!Array.isArray(piece)) {
+        return piece
+      }
+
+      const hasVNodes = piece.some(part => part.hasOwnProperty('componentOptions'))
+      if (hasVNodes === false) {
+        return piece.join('')
+      }
+      return piece
+    })
+
+    return result
   }
 }
