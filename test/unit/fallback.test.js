caf9b9ad2c75f4de7624edeb3f80d903bacea3be
Michael Mokroß <1681227+mmokross@users.noreply.github.com>
2020-04-03 08:52:19 +0200

    refactor fallback tests (#2)

--- test/unit/fallback.test.js
+++ test/unit/fallback.test.js
@@ -1,241 +1,79 @@
 import messages from './fixture/index'
 
 describe('=== #2 Fallback Locale as array for cascading fallbacks ...', () => {
-  describe('... none', () => {
-    let i18n
-    beforeEach(() => {
-      i18n = new VueI18n({
-        locale: 'en',
-        fallbackLocale: false,
-        messages,
-        modifiers: {
-          custom: str => str.replace(/[aeiou]/g, 'x')
-        }
+  var types = [
+    {
+      description: '... none',
+      fallbackLocale: false,
+      tests: [
+        { description: 'English', locale: 'en', expected: ['en'] },
+        { description: 'English (Great Britain)', locale: 'en-GB', expected: ['en-GB', 'en'] },
+        { description: 'German', locale: 'de', expected: ['de'] },
+        { description: 'German (Switzerland)', locale: 'de-CH', expected: ['de-CH', 'de'] }
+      ]
+    },
+    {
+      description: '... simple',
+      fallbackLocale: 'en',
+      tests: [
+        { description: 'English', locale: 'en', expected: ['en'] },
+        { description: 'English (Great Britain)', locale: 'en-GB', expected: ['en-GB', 'en'] },
+        { description: 'German', locale: 'de', expected: ['de', 'en'] },
+        { description: 'German (Switzerland)', locale: 'de-CH', expected: ['de-CH', 'de', 'en'] }
+      ]
+    },
+    {
+      description: '... array',
+      fallbackLocale: ['en', 'ja'],
+      tests: [
+        { description: 'English', locale: 'en', expected: ['en', 'ja'] },
+        { description: 'English (Great Britain)', locale: 'en-GB', expected: ['en-GB', 'en', 'ja'] },
+        { description: 'German', locale: 'de', expected: ['de', 'en', 'ja'] },
+        { description: 'German (Switzerland)', locale: 'de-CH', expected: ['de-CH', 'de', 'en', 'ja'] },
+        { description: 'Japanese', locale: 'ja', expected: ['ja', 'en'] }
+      ]
+    },
+    {
+      description: '... complex',
+      fallbackLocale: {
+        'de-CH': ['fr', 'it'],
+        'zh-Hant': ['zh-Hans'],
+        'es-CL': ['es-AR'],
+        'es': ['en-GB'],
+        'pt': ['es-AR'],
+        'default': ['en', 'da']
+      },
+      tests: [
+        { description: 'German (Switzerland)', locale: 'de-CH', expected: ['de-CH', 'fr', 'it', 'en', 'da'] },
+        { description: 'German (Switzerland) EXACT', locale: 'de-CH!', expected: ['de-CH', 'fr', 'it', 'en', 'da'] },
+        { description: 'German', locale: 'de', expected: ['de', 'en', 'da'] },
+        { description: 'Traditional Chinese', locale: 'zh-Hant', expected: ['zh-Hant', 'zh-Hans', 'zh', 'en', 'da'] },
+        { description: 'Spanish (Spain)', locale: 'es-SP', expected: ['es-SP', 'es', 'en-GB', 'en', 'da'] },
+        { description: 'Spanish (Spain) EXACT', locale: 'es-SP!', expected: ['es-SP', 'en', 'da'] },
+        { description: 'French', locale: 'fr', expected: ['fr', 'en', 'da'] },
+        { description: 'Portuguese (Brazil)', locale: 'pt-BR', expected: ['pt-BR', 'pt', 'es-AR', 'es', 'en-GB', 'en', 'da'] },
+        { description: 'Spanish (Chile)', locale: 'es-CL', expected: ['es-CL', 'es-AR', 'es', 'en-GB', 'en', 'da'] }
+      ]
+    }
+  ]
+  types.forEach(function (type) {
+    describe(type.description, () => {
+      let i18n
+      beforeEach(() => {
+        i18n = new VueI18n({
+          locale: 'en',
+          fallbackLocale: type.fallbackLocale,
+          messages,
+          modifiers: {
+            custom: str => str.replace(/[aeiou]/g, 'x')
+          }
+        })
       })
-    })
-
-    describe('English', () => {
-      const locale = 'en'
-      const expected = ['en']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('English (Great Britain)', () => {
-      var locale = 'en-GB'
-      var expected = ['en-GB', 'en']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('German', () => {
-      var locale = 'de'
-      var expected = ['de']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('German (Switzerland)', () => {
-      var locale = 'de-CH'
-      var expected = ['de-CH', 'de']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-  })
-
-  describe('... simple', () => {
-    let i18n
-    beforeEach(() => {
-      i18n = new VueI18n({
-        locale: 'en',
-        fallbackLocale: 'en',
-        messages,
-        modifiers: {
-          custom: str => str.replace(/[aeiou]/g, 'x')
-        }
-      })
-    })
-
-    describe('English', () => {
-      var locale = 'en'
-      var expected = ['en']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('English (Great Britain)', () => {
-      var locale = 'en-GB'
-      var expected = ['en-GB', 'en']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('German', () => {
-      var locale = 'de'
-      var expected = ['de', 'en']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('German (Switzerland)', () => {
-      var locale = 'de-CH'
-      var expected = ['de-CH', 'de', 'en']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-  })
-
-  describe('... array', () => {
-    let i18n
-    beforeEach(() => {
-      i18n = new VueI18n({
-        locale: 'en',
-        fallbackLocale: ['en', 'ja'],
-        messages,
-        modifiers: {
-          custom: str => str.replace(/[aeiou]/g, 'x')
-        }
-      })
-    })
-
-    describe('English', () => {
-      var locale = 'en'
-      var expected = ['en', 'ja']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('English (Great Britain)', () => {
-      var locale = 'en-GB'
-      var expected = ['en-GB', 'en', 'ja']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('German', () => {
-      var locale = 'de'
-      var expected = ['de', 'en', 'ja']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('German (Switzerland)', () => {
-      var locale = 'de-CH'
-      var expected = ['de-CH', 'de', 'en', 'ja']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('Japanese', () => {
-      var locale = 'ja'
-      var expected = ['ja', 'en']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-  })
-
-  describe('... complex', () => {
-    let i18n
-    beforeEach(() => {
-      i18n = new VueI18n({
-        locale: 'en',
-        fallbackLocale: {
-          'de-CH': ['fr', 'it'],
-          'zh-Hant': ['zh-Hans'],
-          'es-CL': ['es-AR'],
-          'es': ['en-GB'],
-          'pt': ['es-AR'],
-          'default': ['en', 'da']
-        },
-        messages,
-        modifiers: {
-          custom: str => str.replace(/[aeiou]/g, 'x')
-        }
-      })
-    })
-
-    describe('German (Switzerland)', () => {
-      var locale = 'de-CH'
-      var expected = ['de-CH', 'fr', 'it', 'en', 'da']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('German (Switzerland) EXACT', () => {
-      var locale = 'de-CH!'
-      var expected = ['de-CH', 'fr', 'it', 'en', 'da']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('German', () => {
-      var locale = 'de'
-      var expected = ['de', 'en', 'da']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('Traditional Chinese', () => {
-      var locale = 'zh-Hant'
-      var expected = ['zh-Hant', 'zh-Hans', 'zh', 'en', 'da']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('Spanish (Spain)', () => {
-      var locale = 'es-SP'
-      var expected = ['es-SP', 'es', 'en-GB', 'en', 'da']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('Spanish (Spain) EXACT', () => {
-      var locale = 'es-SP!'
-      var expected = ['es-SP', 'en', 'da']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('French', () => {
-      var locale = 'fr'
-      var expected = ['fr', 'en', 'da']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('Portuguese (Brazil)', () => {
-      var locale = 'pt-BR'
-      var expected = ['pt-BR', 'pt', 'es-AR', 'es', 'en-GB', 'en', 'da']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
-      })
-    })
-
-    describe('Spanish (Chile)', () => {
-      var locale = 'es-CL'
-      var expected = ['es-CL', 'es-AR', 'es', 'en-GB', 'en', 'da']
-      it(locale + ' should fallback to ' + expected, () => {
-        assert.deepEqual(i18n.getLocaleChain(locale), expected)
+      type.tests.forEach(function (test) {
+        it(test.description + ': ' + test.locale + ' should fallback to ' + test.expected, () => {
+          // console.log(test.locale + ': --> ' + i18n.getLocaleChain(test.locale))
+          assert.deepEqual(i18n.getLocaleChain(test.locale), test.expected)
+        })
       })
     })
   })
