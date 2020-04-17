0c91a0d9e8a32274b1fe9a33c7e5892512a847f0
exoego <mogami@exoego.net>
2020-04-17 16:58:37 +0900

    Use no-autofix on prefer-const, which conflicts with ie11/no-for-in-const

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
   },
-  plugins: ['flowtype', 'ie11'],
+  plugins: ['flowtype', 'ie11', 'no-autofix'],
   extends: [
     'plugin:vue-libs/recommended',
     'plugin:flowtype/recommended'
@@ -13,6 +13,8 @@ module.exports = {
     'ie11/no-for-in-const': [ 'error' ],
     'ie11/no-loop-func': [ 'warn' ],
     'ie11/no-weak-collections': [ 'error' ],
+    'prefer-const': 'off',
+    'no-autofix/prefer-const': 'warn',
     'object-curly-spacing': ['error', 'always'],
     'no-multiple-empty-lines': ['error', { max: 2, maxBOF: 1 }]
   }
